8608f3156ad7bafaeec08a5bdf22cc4c13ca46cd
DruChill <73948926@elp.edu.pe>
2025-01-05 15:38:48 -0500

    Add dotenv-java dependency and configure datasource properties from environment variables

--- src/main/java/net/enjoy/springboot/registrationlogin/RegistrationLoginApplication.java
+++ src/main/java/net/enjoy/springboot/registrationlogin/RegistrationLoginApplication.java
@@ -3,10 +3,19 @@ package net.enjoy.springboot.registrationlogin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class RegistrationLoginApplication {
 
     public static void main(String[] args) {
+
+        //agrega esto importante
+        Dotenv dotenv = Dotenv.load();
+                System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+                System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        //hasta aquí
+
         SpringApplication.run(RegistrationLoginApplication.class, args);
     }
 }
